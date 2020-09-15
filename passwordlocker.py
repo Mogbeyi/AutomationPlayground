022328275a38111ca3f80d638414e8565b7526c4
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-09-15 16:49:51 +0100

    Implement password locker

---
+++ passwordlocker.py
@@ -0,0 +1,23 @@
+# pw.py - An insecure password locker program.
+import sys, pyperclip
+
+# These are pseudo passwords
+PASSWORDS = {
+	'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+	'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+	'luggage': '12345'
+}
+
+if len(sys.argv) < 2:
+	print('Usage: python pw.py [account] - copy account password')
+	sys.exit()
+
+account = sys.argv[1].lower()
+password = PASSWORDS.get(account, False)
+
+if password:
+	pyperclip.copy(PASSWORDS[account])
+	print('Password for ' + account + ' copied to clipboard.')
+else:
+	print(f"There is no account named {account}")
+
